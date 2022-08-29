50f6f9724b430f735843c107050b0e35e3509732
Faizan <f24aalam@gmail.com>
2022-08-29 21:18:08 +0530

    Changed to Resource Route

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('students/create', [StudentController::class, 'create']);
-Route::get('students', [StudentController::class, 'index']);
-Route::post('students', [StudentController::class, 'store']);
-Route::get('students/{id}', [StudentController::class, 'show']);
-Route::put('students/{id}', [StudentController::class, 'update']);
-Route::delete('/students/{id}', [StudentController::class, 'destroy']);
-Route::get('students/{id}/edit', [StudentController::class, 'edit']);
+// Route::get('students/create', [StudentController::class, 'create']);
+// Route::get('students', [StudentController::class, 'index']);
+// Route::post('students', [StudentController::class, 'store']);
+// Route::get('students/{id}', [StudentController::class, 'show']);
+// Route::put('students/{id}', [StudentController::class, 'update']);
+// Route::delete('/students/{id}', [StudentController::class, 'destroy']);
+// Route::get('students/{id}/edit', [StudentController::class, 'edit']);
+
+Route::resource('students', StudentController::class);
